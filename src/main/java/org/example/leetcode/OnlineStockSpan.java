cb400ee0ff15d760a6f0ed942ef77689e7bed7d8
zerom-h <cupdisin@gmail.com>
2023-08-30 18:33:54 +0900

    add: 문제 주석 추가

--- src/main/java/org/example/leetcode/OnlineStockSpan.java
+++ src/main/java/org/example/leetcode/OnlineStockSpan.java
@@ -2,6 +2,11 @@ package org.example.leetcode;
 
 import java.util.Stack;
 
+/**
+ * Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+ * The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
+ *
+ */
 public class OnlineStockSpan {
     Stack<int[]> ints;
     public OnlineStockSpan() {
